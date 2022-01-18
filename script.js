2c88f5c0d3a1d533d3f4f680c8ad990550272c45
Carlos Zaldívar Bent <bentsoft365@gmail.com>
2022-01-18 17:15:14 -0500

    testing copied code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('console working')
+
+document.getElementById('figuras').contentEditable = 'true';
+    document.getElementById('figuras').designMode='on';
+    $(function() {
+        $("#btnSave2").click(function() {
+        html2canvas($("#figuras"), {
+            onrendered: function(canvas) {
+            saveAs(canvas.toDataURL(), 'Fancyxt.png');
+            }
+        });
+        });
+        function saveAs(uri, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
+        }
+    });
